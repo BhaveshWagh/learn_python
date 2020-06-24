217e023d9f28a3db55b53fe0592f55e00d1b7ffb
BhaveshWagh2711 <bhaveshbm2711@gmail.com>
2020-06-24 23:45:16 +0530

    fun_sandwich

---
+++ fun_sandwich.py
@@ -0,0 +1,8 @@
+def oredered_sandwich(*items):
+    print("\n I will done your sandwich order")
+    print("That is .....")
+    for name in items:
+        print(f" {name} sandwich is ready")
+oredered_sandwich("paneer", "veg shammi", "tunna")
+oredered_sandwich("chicken", "veg", "tomato")
+oredered_sandwich("onion", "corn & peas", "chopped chees")
