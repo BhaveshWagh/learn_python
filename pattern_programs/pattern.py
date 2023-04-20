ca61d8d9d0b6d9b095149f24f43a8862940694a3
BhaveshWagh <bhaveshbm2711@gmail.com>
2023-04-20 23:24:19 +0530

    pattern Programs

--- pattern_programs/pattern.py
+++ pattern_programs/pattern.py
@@ -1,2 +1,24 @@
-for i in range(1, 5, n-1):
-    print(1*i)
+# Print the following pattern
+# 12345
+# 12345
+# 12345
+# 12345
+# 12345
+for i in range(5):
+    for j in range(1,6):
+        print(j,end="")
+    print()
+
+print("-*-*-" * 25)
+
+# Print the following pattern
+# 1
+# 22
+# 333
+# 4444
+# 55555
+for i in range(1, 6):
+    for j in range(i):
+        print(i,end="")
+    print()
+
