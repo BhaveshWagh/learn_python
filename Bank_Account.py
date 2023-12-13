e9942c8e8bddafec18dfa485e760b5fb02fdac4f
BhaveshWagh <bhaveshbm2711@gmail.com>
2023-12-13 18:25:36 +0530

    bank Account problem

--- Bank_Account.py
+++ Bank_Account.py
@@ -12,5 +12,18 @@ class BankAccount():
         if amount < 0:
             return
         if amount > self.balance:
-            return
-        self.balance = self.balance - amount
+            print(f"${amount} This withdrawal will not be allowed due to insufficient funds")
+        self.balance -= amount
+
+account = BankAccount("John Doe")
+print(f"Initial balance: ${account.balance}")
+
+account.deposit(100.00)
+print(f"Balance after deposit: ${account.balance}")
+
+account.withdraw(50.00)
+print(f"Balance after withdrawal: ${account.balance}")
+
+account.withdraw(80.00)  # This withdrawal will not be allowed due to insufficient funds
+print(f"Balance after attempted withdrawal: ${account.balance}")
+
