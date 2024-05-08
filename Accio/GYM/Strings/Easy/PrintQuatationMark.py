38c5729d2dae58f24cf6ceec06ec8b7eb2813300
BhaveshWagh <bhaveshbm2711@gmail.com>
2024-05-08 10:27:09 +0530

    PrintQuatationMark

---
+++ Accio/GYM/Strings/Easy/PrintQuatationMark.py
@@ -0,0 +1,11 @@
+# class Solution:
+def addQuotationMark(s):
+    #Write your code here
+    output = f'"{s}"'
+    return output
+s = "My name is Bhavesh"  
+print(addQuotationMark(s))
+# if __name__=='__main__':
+#     s=input()
+#     Obj = Solution()
+#     print(Obj.addQuotationMark(s))
